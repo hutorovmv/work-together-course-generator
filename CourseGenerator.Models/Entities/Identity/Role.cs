ebca2cc2895edf00175cde79548cb2dff02a33eb
Mykola <hutorovmv@gmail.com>
2020-03-10 01:02:54 +0200

    feat(Models): Add models for user and role

---
+++ CourseGenerator.Models/Entities/Identity/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace CourseGenerator.Models.Entities.Identity
+{
+    public class Role : IdentityRole
+    {
+    }
+}
---
+++ CourseGenerator.Models/Entities/Identity/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace CourseGenerator.Models.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
