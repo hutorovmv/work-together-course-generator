89597db393f28977c58f0e5f8f7d3096d4a4f834
Mykola <hutorovmv@gmail.com>
2020-03-10 23:19:45 +0200

    feat(DAL): Add base functionality for pagination
    Functionality included:
    - IPagedList (interface that provides information about page)
    - PagedList (generic class that contains information as well as items
    of specified type)
    - PaginationExtensions (contains methods for creating PagedList from IQueryable or IEnumerable)
    
    Resolves: #7

---
+++ CourseGenerator.DAL/Pagination/IPagedList.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseGenerator.DAL.Pagination
+{
+    public interface IPagedList
+    {
+        int TotalPages { get; }
+        int TotalCount { get; }
+        int PageIndex { get; }
+        int PageSize { get; }
+        bool HavePreviousPage { get; }
+        bool HaveNextPage { get; }
+    }
+}
---
+++ CourseGenerator.DAL/Pagination/PaginationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; 
+
+namespace CourseGenerator.DAL.Pagination
+{
+    public static class PaginationExtensions
+    {
+        public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> items, int pageSize, int pageIndex)
+        {
+            int totalCount = items.Count();
+            IEnumerable<T> itemsCollection = await items.Skip(pageSize * (pageIndex - 1)).Take(pageSize).ToListAsync();
+            return new PagedList<T>(itemsCollection, totalCount, pageSize, pageIndex);
+        }
+
+        public static PagedList<T> ToPagedList<T>(this IEnumerable<T> items, int pageSize, int pageIndex)
+        {
+            int totalCount = items.Count();
+            IEnumerable<T> itemsCollection = items.Skip(pageSize * (pageIndex - 1)).Take(pageSize).ToList();
+            return new PagedList<T>(itemsCollection, totalCount, pageSize, pageIndex);
+        }
+    }
+}
