ee5b865116bafbe0782ab1d8ed06b5b8e67ba95c
Mykola <hutorovmv@gmail.com>
2020-03-23 12:52:54 +0200

    feat(DAL): Add generic repository and UoW
    - IRepository contains basic repository functionality
    - IGenericEFRepository contains functionality for EF repository
    - GenericEFRepository - implementation of IGenericEFRepository
    - IUnitOfWork interface for UoW
    - UnitOfWork - implementation of UnitOfWork

---
+++ CourseGenerator.BLL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseGenerator.BLL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task SaveAsync();
+    }
+}
