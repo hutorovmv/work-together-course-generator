7ee6ac993a0acb1e2ddece6ca54a08d3071e0bc5
andrew <ryzhkovandruha@gmail.com>
2020-04-30 14:07:55 +0300

    feat(DAL): Add new models for documenal data base
    Add models Article and ArticleLang for work with mongodb

---
+++ CourseGenerator.Models/Entities/Content/Article.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseGenerator.Models.Entities.Content
+{
+    public class Article
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string Author { get; set; }
+
+        [BsonIgnoreIfDefault]
+        public DateTime LastEdited { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string LastEditedBy { get; set; }
+
+        [BsonIgnoreIfNull]
+        public List<string> SourceUris { get; set; }
+
+        public ICollection<ArticleLang> Localization { get; set; }
+    }
+}
---
+++ CourseGenerator.Models/Entities/Content/ArticleLang.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseGenerator.Models.Entities.Content
+{
+    public class ArticleLang
+    {
+        public string LangCode { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string Heading { get; set; }
+
+        [BsonIgnoreIfDefault]
+        public ObjectId FileId { get; set; }
+    }
+}
